2d80b2cc02d5ac79cec29ad07fde959e99eb0868
Abu Hanif Arnob <hanifcse90@gmail.com>
2022-10-25 23:32:17 +0600

    All Data add

--- index.js
+++ index.js
@@ -3,8 +3,29 @@ const cors = require("cors");
 const app = express();
 app.use(cors())
 const port = process.env.PORT || 5000;
+const category = require("./data/category.json")
+const courses = require("./data/cours.json")
 
 
 app.get("/", (req, res) => {
-    res.send("Cours API Running")
+    res.send("Courses API Running")
+})
+
+app.get("/course-category", (req, res) => {
+    res.send(category);
+})
+app.get("/category/:id", (req, res) => {
+    const id = req.params.id;
+    if (id === '07') {
+        res.send(courses)
+    } else {
+        const categoryCourses = courses.filter(cours => cours.category_id === id)
+        res.send(categoryCourses)
+    }
+
+
+})
+
+app.listen(port, () => {
+    console.log("Dragon surver running", port);
 })
